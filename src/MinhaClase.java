72b335c2808cb066d0c7c5b3e67b36f2586cd39b
Natanael Amaral <natan.amaral.barros@gmail.com>
2023-08-29 15:24:03 -0300

    novo arquivo de estudo

---
+++ src/MinhaClase.java
@@ -0,0 +1,14 @@
+public class MinhaClase {
+    public static void main(String[] args) {
+        String primeiroNome = "Natanael";
+        String segundoNome = "Amaral";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeriNome, String segundoNome ){
+        return "Resultado do método" + " " + primeriNome.concat(" ").concat(segundoNome);
+    }
+}
